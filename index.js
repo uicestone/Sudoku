a71f6704342fb653b3a18bb1d3ea096aa0457802
Uice Lu <uicestone@gmail.com>
2017-05-31 16:53:15 +0800

    递归求解, 每层从最少可能格开始

--- index.js
+++ index.js
@@ -1,113 +1,170 @@
+const start = new Date();
 const size = 9;
 const blockSize = 3;
 const values = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-const hints = [[4, 0, 1], [5, 0, 4], [7, 0, 8], [0, 1, 7], [2, 1, 8], [5, 2, 8], [0, 3, 5], [2, 3, 2], [3, 4, 6], [6, 5, 1], [8, 5, 8], [6, 6, 9], [1, 7, 6], [3, 8, 9], [4, 8, 3]];
+const hints = new Set([[0, 4, 1], [0, 5, 4], [0, 7, 8], [1, 0, 7], [1, 2, 8], [2, 5, 8], [3, 0, 5], [3, 2, 2], [4, 3, 6], [5, 6, 1], [5, 8, 8], [6, 6, 9], [7, 1, 6], [8, 3, 9], [8, 4, 3]]);
 
 /* Cell:
 {
-	value: Number,
 	possibleValues: [Number],
-	sure: Boolean
+	row: Number,
+	column: Number,
+	block: Number,
+	value: Number,
 }
 */
 
-let rows = []; // [[Cell]]
-let columns = []; // [[Cell]]
-let blocks = []; // [[Cell]]
+/* Sudoku: 
+{
+	cells: [Cell],
+	rows: [[Cell]],
+	columns: [[Cell]],
+	blocks: [[Cell]]
+}
+*/
 
-initializeGroups();
-initializeHints();
-printSudoku();
-linkCellsToColumnsAndBlocks();
+try {
+	solve(hints);
+}
+catch (e) {
+	console.log('Solved.');
+}
 
-setGroupsPossibleValues();
-
-rows.forEach((row, y) => {
-	row.forEach((cell, x) => {
-		console.log(y, x, cell);
-	});
-});
+console.log('Time:', new Date() - start);
 
 
 // functions
 
-function initializeGroups() {
-	// initialize size of rows, columns and blocks
-	for (let y = 0; y < size; y++) {
-		
-		rows.push([]);
-		columns.push([]);
-		blocks.push([]);
+function solve (filledValues) {
 
-		for (let x = 0; x < size; x++) {
-			rows[y].push({});
-		}
+	let sudoku = initializeSudoku(filledValues);
+
+	// if (new Date().getTime() % 100 === 0)
+	printSudoku(sudoku, [...filledValues][filledValues.size - 1]);
+	console.log('Filled:', filledValues.size);
+
+	if (filledValues.size === 81) {
+		throw 'solved';
 	}
-}
 
-function initializeHints() {
-	hints.forEach(hint => {
-		rows[hint[1]][hint[0]] = {value: hint[2], sure: true, possibleValues: new Set([hint[2]])};
-	});
-}
+	sudoku.cells.filter(cell => !cell.value).sort((a, b) => a.possibleValues.size - b.possibleValues.size).forEach(cell => {
+		
+		cell.possibleValues.forEach(value => {
+			
+			let newFilledValues = new Set(filledValues.values());
+			newFilledValues.add([cell.row, cell.column, value]);
 
-function linkCellsToColumnsAndBlocks () {
-	rows.forEach((row, y) => {
-		row.forEach((cell, x) => {
-			// link cell to column
-			columns[x][y] = cell;
+			try {
+				solve(newFilledValues);
+			}
 
-			// link cell to block
-			let blockNumber = Math.floor(x / blockSize) + Math.floor(y / blockSize) * blockSize;
-			let cellNumberInBlock = x % blockSize + y % blockSize * blockSize;
-			blocks[blockNumber][cellNumberInBlock] = cell;
+			catch (e) {
+				if (e === 'solved') {
+					throw e;
+				}
+			}
 		});
 	});
 }
 
-function printSudoku() {
-	process.stdout.write("\033c");
+// rows.forEach((row, y) => {
+// 	row.forEach((cell, x) => {
+// 		console.log(y, x, cell);
+// 	});
+// });
+
+
+// functions
+
+function initializeSudoku(filledValues) {
+
+	let sudoku = {rows: [], columns: [], blocks: [], cells: []};
+	// initialize size of rows, columns and blocks
+	for (let y = 0; y < size; y++) {
+		
+		sudoku.rows.push([]);
+		sudoku.columns.push([]);
+		sudoku.blocks.push([]);
+
+		for (let x = 0; x < size; x++) {
+			let cell = {possibleValues: values, row: y, column: x};
+			sudoku.rows[y].push(cell);
+			sudoku.cells.push(cell);
+		}
+	}
+
+	initializeFilledValues(sudoku, filledValues);
+	linkCellsToColumnsAndBlocks(sudoku);
+	setGroupsPossibleValues(sudoku);
+
+	return sudoku;
+}
+
+function initializeFilledValues(sudoku, filledValues) {
+	filledValues.forEach(value => {
+		let cell = sudoku.rows[value[0]][value[1]];
+		cell.value = value[2];
+		cell.possibleValues = new Set([value[2]]);
+	});
+}
+
+function linkCellsToColumnsAndBlocks (sudoku) {
+	sudoku.cells.forEach(cell => {
+		// link cell to column
+		sudoku.columns[cell.column][cell.row] = cell;
+
+		// link cell to block
+		let blockNumber = Math.floor(cell.column / blockSize) + Math.floor(cell.row / blockSize) * blockSize;
+		let cellNumberInBlock = cell.column % blockSize + cell.row % blockSize * blockSize;
+		sudoku.blocks[blockNumber][cellNumberInBlock] = cell;
+		cell.block = blockNumber;
+	});
+}
+
+function setGroupsPossibleValues (sudoku) {
+	sudoku.rows.forEach(row => setCellsPossibleValues(row));
+	sudoku.columns.forEach(column => setCellsPossibleValues(column));
+	sudoku.blocks.forEach(block => setCellsPossibleValues(block));
+}
+
+function setCellsPossibleValues(group) {
+
+	const groupFilledValues = new Set(group.filter(cell => cell.value).map(cell => cell.value));
+	const possibleValues = new Set([...values].filter(value => !groupFilledValues.has(value)));
+
+	group.forEach(cell => {
+
+		if (cell.value) {
+			return;
+		}
+
+		cell.possibleValues = new Set([...cell.possibleValues].filter(value => possibleValues.has(value)));
+
+		if (cell.possibleValues.size === 0) {
+			throw 'no possible value';
+		}
+	});
+}
+
+function printSudoku(sudoku, testing) {
+	// process.stdout.write("\033c");
 	process.stdout.write("\n\n" + '-'.repeat(4 * size + 1) + "\n");
-	rows.forEach((row, y) => {
+	sudoku.rows.forEach((row, y) => {
 		row.forEach((cell, x) => {
 			// print sudoku
 			process.stdout.write((x ? '' : '|') + ' ');
-			process.stdout.write((cell.value ? String(cell.value) : ' '));
+			if (cell.row === testing[0] && cell.column === testing[1]) {
+				process.stdout.write(`\x1b[36m${cell.value}\x1b[0m`);
+			}
+			else if (cell.value) {
+				process.stdout.write(String(cell.value));
+			}
+			else {
+				process.stdout.write(' ');
+			}
 			process.stdout.write(' |');
 		});
 		process.stdout.write("\n" + '-'.repeat(4 * size + 1) + "\n");
 	});
 }
 
-function setCellsPossibleValues(group) {
-	const groupSureValues = new Set(group.filter(cell => cell.sure).map(cell => cell.value));
-	const possibleValues = new Set([...values].filter(value => !groupSureValues.has(value)));
-	
-	group.forEach(cell => {
-
-		if (cell.sure) {
-			return;
-		}
-
-		if (!cell.possibleValues) {
-			cell.possibleValues = possibleValues;
-		}
-		else {
-			cell.possibleValues = new Set([...cell.possibleValues].filter(value => possibleValues.has(value)));
-		}
-
-		if (cell.possibleValues.size === 0) {
-			throw 'not possible';
-		}
-		else if (cell.possibleValues.size === 1) {
-			cell.sure = true;
-			cell.value = [...cell.possibleValues][0];
-		}
-	});
-}
-
-function setGroupsPossibleValues () {
-	rows.forEach(row => setCellsPossibleValues(row));
-	columns.forEach(column => setCellsPossibleValues(column));
-	blocks.forEach(block => setCellsPossibleValues(block));
-}
